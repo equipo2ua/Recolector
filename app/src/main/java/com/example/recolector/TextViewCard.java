4c4188ac08aa57409d87e44becb8aefa02ae3503
DiegoAC <diego.antivil.curaqueo@gmail.com>
2020-07-05 00:15:11 -0400

    Creacion componente TextViewCard

---
+++ app/src/main/java/com/example/recolector/TextViewCard.java
@@ -0,0 +1,44 @@
+package com.example.recolector;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+
+public class TextViewCard extends LinearLayout {
+
+    private TextView idSolicitud, distance , dateSolicitud;
+
+    public TextViewCard(Context context) {
+        super(context);
+        initChilds();
+    }
+
+    public TextViewCard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initChilds();
+    }
+    private void initChilds(){
+        distance = new TextView(getContext());
+        idSolicitud = new TextView(getContext());
+        dateSolicitud = new TextView(getContext());
+        distance.setGravity(Gravity.LEFT);
+        idSolicitud.setGravity(Gravity.LEFT);
+        dateSolicitud.setGravity(Gravity.LEFT);
+        addView(distance);
+        addView(idSolicitud);
+        addView(dateSolicitud);
+    }
+
+    private void setAppearence(){
+
+    }
+
+    public void setCardText(int id, int dist, String date){
+        idSolicitud.setText(String.valueOf(id));
+        distance.setText(String.valueOf(dist));
+        dateSolicitud.setText(date);
+    }
+}
