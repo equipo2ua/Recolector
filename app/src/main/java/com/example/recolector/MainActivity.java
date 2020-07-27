c575d036f53f19078670ba0b0ca19bbb5d6c045e
DiegoAC <diego.antivil.curaqueo@gmail.com>
2020-07-27 00:46:13 -0400

    Menu Lateral ok

--- app/src/main/java/com/example/recolector/MainActivity.java
+++ app/src/main/java/com/example/recolector/MainActivity.java
@@ -1,21 +1,54 @@
 package com.example.recolector;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Switch;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setToolbar();
+        drawerLayout =findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navView);
 
-        Intent intencion = new Intent(MainActivity.this,IniciarSesion.class);
+       Intent intencion = new Intent(MainActivity.this,IniciarSesion.class);
         startActivity(intencion);
+
+    }
+
+    private void setToolbar(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
